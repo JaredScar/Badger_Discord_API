b000873f3d310cf66e158585750ecb733389dd76
Jared <TheWolfBadger@gmail.com>
2021-08-26 22:40:53 -0400

    Added Zap-Hosting advertisements

---
+++ fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'JaredScar'
+description 'Badger\'s Discord API'
+version '1.6'
+url 'https://github.com/JaredScar/Badger_Discord_API'
+
+client_scripts {
+	'client.lua',
+}
+
+server_scripts {
+	'config.lua',
+	"server.lua", -- Uncomment this line
+	--"example.lua" -- Remove this when you actually start using the script!!!
+}
+
+server_exports { 
+	"GetDiscordRoles",
+	"GetRoleIdfromRoleName",
+	"GetDiscordAvatar",
+	"GetDiscordName",
+	"GetDiscordEmail",
+	"IsDiscordEmailVerified",
+	"GetDiscordNickname",
+	"GetGuildIcon",
+	"GetGuildSplash",
+	"GetGuildName",
+	"GetGuildDescription",
+	"GetGuildMemberCount",
+	"GetGuildOnlineMemberCount",
+	"GetGuildRoleList",
+	"ResetCaches",
+	"CheckEqual"
+} 
